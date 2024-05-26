504ad23af714911048adb2d204550ff748af0a72
Goto15 <ben.sullivan@hey.com>
2024-05-26 10:25:01 -0500

    Sanitize json, parse into Swift with ruby script

--- JustWorkouts/Data/parse_json_to_swift.rb
+++ JustWorkouts/Data/parse_json_to_swift.rb
@@ -3,23 +3,23 @@ require 'Set'
 
 data_hash = JSON.parse(File.read('exercises.json'))
 
-prepend_text = "static let exercisesData = [\n"
+prepend_text = "let exercisesData = [\n"
 postpend_text = "]"
 
 exercisesArray = data_hash['exercises'].map do |exercise|
     "Exercise( name: \"#{exercise['name']}\",
+                primaryMuscles: \"#{exercise['primaryMuscles'].join(',')}\",
+                secondaryMuscles: \"#{exercise['secondaryMuscles'].join(',')}\",
                 force: \"#{exercise['force']}\",
                 level: \"#{exercise['level']}\",
                 mechanic: \"#{exercise['mechanic']}\",
                 equipment: \"#{exercise['equipment']}\",
-                primary_muscles: \"#{exercise['primaryMuscles'].join(',')}\",
-                secondary_muscles: \"#{exercise['secondaryMuscles'].join(',')}\",
-                instructions: \"#{exercise['instructions'].join('.')}\",
                 category: \"#{exercise['category']}\",
+                instructions: \"#{exercise['instructions'].join(' ')}\"
     ),\n"
 end
 
 full_text = prepend_text + exercisesArray.join + postpend_text
 
-File.open("exercises.swift", 'w') { |file| file.write(full_text)}
+File.open("ExercisesList.swift", 'w') { |file| file.write(full_text)}
 puts 'Done'
