41fc29a44e6875bfd0746d1ca86fc8ae7240ef5f
Goto15 <ben.sullivan@hey.com>
2024-05-26 10:50:43 -0500

    Refactor Exercise and exerciseList into own file, create exercise list dropdown

---
+++ JustWorkouts/Components/ExerciseSelectDropdown.swift
@@ -0,0 +1,18 @@
+//
+//  ExerciseSelectDropdown.swift
+//  JustWorkouts
+//
+//  Created by ben on 5/26/24.
+//
+
+import SwiftUI
+
+struct ExerciseSelectDropdown: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExerciseSelectDropdown()
+}
---
+++ JustWorkouts/Models/Exercises.swift
@@ -0,0 +1,8 @@
+//
+//  Exercises.swift
+//  JustWorkouts
+//
+//  Created by ben on 5/26/24.
+//
+
+import Foundation
